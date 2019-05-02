83a48b2404599eb84238f5e376ebfd2f6f6de892
srikanthkatta2207 <srikanthkatta2207@gmail.com>
2019-05-02 18:17:14 +0530

    [Srikanth] Add conent for process

--- src/data/process.js
+++ src/data/process.js
@@ -1,24 +1,26 @@
 const processData = [
-    {
-        icon: 'remote',
-        label: 'Assistance',
-        description: 'Assistance from the remote software developers'
-    },
-    {
-        icon: 'projects ',
-        label: 'Projects',
-        description: 'Real time projects with agile practises'
-    },
-    {
-        icon: 'feedback',
-        label: 'Feedback',
-        description: 'Daily feedback on their skills'
-    },
-    {
-        icon: 'communication',
-        label: 'Communication',
-        description: 'Weekly sessions on communication'
-    }
+  {
+    icon: 'remote',
+    label: 'Assistance',
+    description:
+      'Remote assitance from the people who are in software industry.'
+  },
+  {
+    icon: 'projects ',
+    label: 'Projects',
+    description:
+      'Real time projects with the latest technoloiges and agile practises.'
+  },
+  {
+    icon: 'feedback',
+    label: 'Feedback',
+    description: 'Daily feedback on their technical skills'
+  },
+  {
+    icon: 'communication',
+    label: 'Communication',
+    description: 'Weekly sessions on communication'
+  }
 ];
 
 export default processData;
